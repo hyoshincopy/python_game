fc5195a03fa7f4887aa3ee56915c906e5281bf63
unknown <package9@naver.com>
2020-05-28 09:50:04 +0900

    연승이의 모험 게임

--- game.py
+++ game.py
@@ -1,54 +1,114 @@
 import pygame
+import time
+import random
 
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0, 0, 0)
 white = (255, 255, 255)
-pad_width = 1024
-pad_height = 512
+red = (255, 0, 0)
+
+car_width = 73
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('연승이의 모험')
+clock = pygame.time.Clock()
+
+carImg = pygame.image.load('fly1.png')
 
 
-def ariplane(x, y):
-    global gamepad, aircraft
-    gamepad.blit(aircraft, (x, y))
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
 
 
-def runGame():
-    global gamepad, clock, aircraft
+def car(x, y):
+    gameDisplay.blit(carImg, (x, y))
 
-    x = pad_width*0.05
-    y = pad_height*0.8
-    y_change = 0
-    crashed = False
-    while not crashed:
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2), (display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+
+def crash():
+    message_display('he is dead')
+
+
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+
+    x_change = 0
+
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 7
+    thing_width = 100
+    thing_height = 100
+
+    gameExit = False
+
+    while not gameExit:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                crashed = True
+                pygame.quit()
+                quit()
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    y_change += 5
-                elif event.key == pygame.K_DOWN:
-                    y_change -= 5
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    y_change = 0
-        y += y_change
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
 
-        gamepad.fill(white)
-        ariplane(x, y)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+        x += x_change
+        gameDisplay.fill(white)
+
+        # things(thingx, thingy, thingw, thingh, color)
+        things(thing_startx, thing_starty, thing_width, thing_height, black)
+        thing_starty += thing_speed
+        car(x, y)
+
+        if x > display_width - car_width or x < 0:
+            crash()
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0, display_width)
+
+        ####
+        if y < thing_starty+thing_height:
+            print('y crossover')
+
+            if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
+                print('x crossover')
+                crash()
+        ####
+
         pygame.display.update()
         clock.tick(60)
-    pygame.quit()
-    quit()
 
 
-def initGame():
-    global gamepad, clock, aircraft
-
-    pygame.init()
-    gamepad = pygame.display.set_mode((pad_width, pad_height))
-    pygame.display.set_caption('PyFlying')
-    aircraft = pygame.display.set_caption('./fly.png')
-    clock = pygame.time.Clock()
-    runGame()
-
-
-initGame()
+game_loop()
+pygame.quit()
+quit()
