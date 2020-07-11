12aff33f8382fc81e54e4ffbf03e2f12e800aa5c
park <52433338+hyoshincopy@users.noreply.github.com>
2020-07-11 18:35:39 +0900

    Update game.py

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ display_height = 600
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
-
+aad=12
 car_width = 73
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
